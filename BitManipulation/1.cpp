12dc7db3c1641d92be8ebc7c9b6b9b03dd499263
Abhishek Kumar <97511860+abhi-hash256@users.noreply.github.com>
2022-09-14 02:24:46 +0530

    code to check if kth bit of a no is set or not

--- BitManipulation/1.cpp
+++ BitManipulation/1.cpp
@@ -1,1 +1,45 @@
 
+# Check if kth bit in a number is set or not
+
+1). Using left shift operator
+    
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define pb v.push_back
+
+int main()
+{
+  int n,k;    // n is the input no. and k if the bit to check
+  cin>>n>>k;
+  
+  if(n&(1<<(k-1)) != 0)      // Creating a mask (a no whose kth bit is set by left shifting 1 by k-1)   
+    cout<<"kth bit is set\n";
+  else
+    cout<<"kth bit is not set\n";
+ return 0;
+}
+
+
+2). Using right shift operator
+  
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define pb v.push_back
+
+int main()
+{
+  int n,k;    // n is the input no. and k if the bit to check
+  cin>>n>>k;
+  
+  if(((n>>(k-1))&1)==1)      // Shifting the kth bit of n to rightmost side and do bitwose and with 1
+    cout<<"kth bit is set\n";
+  else
+    cout<<"kth bit is not set\n";
+ return 0;
+}
+
+  

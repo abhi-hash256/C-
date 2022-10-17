602c583909ef76e14f8fa431edc80ed8750e0260
Abhishek Kumar <97511860+abhi-hash256@users.noreply.github.com>
2022-10-18 00:49:13 +0530

    Linked list implementation ( insertion node at last)

--- LinkList/a.cpp
+++ LinkList/a.cpp
@@ -1,1 +1,47 @@
+# Linked List implementation
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node* insert(Node* root, int data){
+    Node* temp = new Node(data);
+    if(root==NULL)
+      return temp;
+    Node* temp1 = root;
+    while(temp1->next!=NULL)
+      temp1 = temp1->next;
+    temp1->next = temp;
+  return root;
+}
+
+void print(Node* root){
+    if(root==nullptr)
+      return;
+    cout<<root->data<<" ";
+    print(root->next);
+}
+
+int main()
+{
+     Node* root = NULL;
+     int n,a;
+     cin>>n;
+     for(int i=0; i<n; i++){
+         cin>>a;
+         root = insert(root,a);
+     }
+     print(root);
+     cout<<"\n";
+  return 0;
+}
